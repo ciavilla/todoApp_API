902e938dd576ea8cb1d8d91a548a32663572e805
ciera <cieravillalpando@gmail.com>
2025-07-29 16:55:37 -0600

    Routes: Fixed an error in tasks after testing with postman

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/', getAllTasks);
 router.post('/', validateCreateTask, createTask);
-router.put('/', validateUpdateTask, updateTask);
+router.put('/:id', validateUpdateTask, updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;
