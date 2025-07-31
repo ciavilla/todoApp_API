788072f5fbd35ac1438109941a21da27e58d87fa
ciera <cieravillalpando@gmail.com>
2025-07-31 17:13:24 -0600

    types:  added colors that were missing in types

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,5 +18,5 @@ export interface UpdateTask {
     completed?: boolean;
 }
 
-export const VALID_COLORS = ['red', 'blue', 'green', 'yellow', 'purple', 'pink'] as const;
+export const VALID_COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink', 'brown'] as const;
 export type TaskColor = typeof VALID_COLORS[number];
